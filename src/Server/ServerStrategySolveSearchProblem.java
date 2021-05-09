79329ffebdcc9ea5b89d380cb81b5d20fd4844a0
Nivfono2001 <81295283+Nivfono2001@users.noreply.github.com>
2021-05-09 15:04:15 +0300

    working on file

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -6,20 +6,38 @@ import algorithms.mazeGenerators.Maze;
 import algorithms.mazeGenerators.MyMazeGenerator;
 import algorithms.search.*;
 
+import java.util.concurrent.*;
 import java.io.*;
 
-public class ServerStrategySolveSearchProblem implements IServerStrategy{
-
+public class ServerStrategySolveSearchProblem implements IServerStrategy {
+    ConcurrentHashMap<byte[], File> concurrentHashMap = new ConcurrentHashMap<>();
+    String tempDirectoryPath = System.getProperty("java.io.tmpdir");
+    volatile int counter=0;
     @Override
     public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
         try {
             ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
             ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
-            Maze toSolveMaze=(Maze)fromClient.readObject();
-            SearchableMaze searchableMaze = new SearchableMaze(toSolveMaze);
+            Maze toSolveMaze = (Maze) fromClient.readObject();
+            byte[] compMaze = toSolveMaze.toByteArray();
+            Solution MazeSolution;
+            File myFile;
+            if (concurrentHashMap.containsKey(compMaze)) {
+                myFile = concurrentHashMap.get(compMaze).getAbsoluteFile();
+                ObjectInputStream fromFile = new ObjectInputStream(new FileInputStream(myFile));
+                MazeSolution = (Solution) fromFile.readObject();
 
-            ISearchingAlgorithm searcher=getSearchingAlgFromConfig();
-            Solution MazeSolution=searcher.solve(searchableMaze);
+            } else {
+                SearchableMaze searchableMaze = new SearchableMaze(toSolveMaze);
+                ISearchingAlgorithm searcher = getSearchingAlgFromConfig();
+                MazeSolution = searcher.solve(searchableMaze);
+                myFile=new File(tempDirectoryPath+"MazeNumber"+counter);
+                System.out.println("++++++++++++++++++++++++++++++++++++++");
+                System.out.println(myFile.getAbsolutePath());
+                System.out.println("++++++++++++++++++++++++++++++++++++++");
+
+                counter++;
+            }
             OutputStream out = new MyCompressorOutputStream(toClient);
             toClient.writeObject(MazeSolution);
             out.flush();
@@ -32,7 +50,7 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
     }
 
     private ISearchingAlgorithm getSearchingAlgFromConfig() {
-        Configurations configurations=Configurations.getInstance();
+        Configurations configurations = Configurations.getInstance();
         try (InputStream input = new FileInputStream("src/resources/config.properties")) {
 
             // load a properties file
@@ -41,9 +59,9 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            if(configurations.getProperties().getProperty("db.mazeSearchingAlg").equals("BestFirstSearch"))
+            if (configurations.getProperties().getProperty("db.mazeSearchingAlg").equals("BestFirstSearch"))
                 return new BestFirstSearch();
-            if(configurations.getProperties().getProperty("db.mazeSearchingAlg").equals("BreadthFirstSearch"))
+            if (configurations.getProperties().getProperty("db.mazeSearchingAlg").equals("BreadthFirstSearch"))
                 return new BestFirstSearch();
 
         } catch (IOException ex) {
